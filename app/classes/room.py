73788d04df8e77baff291e1eadb71167a2bc9d20
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-04 15:06:01 +0100

    feat: room class setup for simulation

---
+++ app/classes/room.py
@@ -0,0 +1,17 @@
+class Room ():
+	"""Represents a room in a building."""
+	from .sensor import Sensor
+	def __init__(self, room_info, crowd_factor: float, area: float, sensors: list[Sensor]):
+		self.id = room_info["id"]
+		self.name = room_info["name"]
+		self.type = room_info["type"]
+		self.occupancy = 0
+		self.crowd_factor = crowd_factor
+		self.area = area
+		self.sensors = sensors
+	
+	def increment_occupancy(self):
+		self.occupancy += 1
+	
+	def decrement_occupancy(self):
+		self.occupancy -= 1
