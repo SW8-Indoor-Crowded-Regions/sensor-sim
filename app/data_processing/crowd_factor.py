c5572f483577c4fdabea585dd3db93bd434be937
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-11 11:03:49 +0100

    test: tests for crowd_factor.py + small fixes

--- app/data_processing/crowd_factor.py
+++ app/data_processing/crowd_factor.py
@@ -1,22 +1,14 @@
-import os
-import sys
 from db import Database
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from app.classes.consumer import Consumer
-from app.utils.loadObjects import load_rooms, load_sensors
+from app.utils.loadObjects import load_rooms
 
 
-
-Database()
-rooms = load_rooms()
-sensors = load_sensors(rooms)
-
-def calculate_crowd_factor(sensor_data: dict[str, dict[str, int]]):
-	"""Calculates the crowdedness of the rooms based on the sensor data.
+def update_room_occupancy(sensor_data: dict[str, dict[str, int]]):
+	"""Updates the room occupancy based on the sensor data.
 	Args:
 		sensor_data (dict): The sensor data.	 
-  """
+	"""
+	rooms = load_rooms()
 	if sensor_data["room1"]["room_id"] == "0":
 		room2 = [room for room in rooms if room.id.__str__() == sensor_data["room2"]["room_id"]][0]
 		room2.add_occupants(sensor_data["room2"]["movements"])
@@ -36,9 +28,6 @@ def calculate_crowd_factor(sensor_data: dict[str, dict[str, int]]):
 def process_sensor_data():
 	""" Processes the sensor data by consuming messages from the sensor-data topic and runs the calculate_crowd_factor function.
 	"""
-	consumer = Consumer(calculate_crowd_factor, "sensor-data")
+	Database()
+	consumer = Consumer(update_room_occupancy, "sensor-data")
 	consumer.consume_messages()
-
-
-if __name__ == "__main__":
-	process_sensor_data()
