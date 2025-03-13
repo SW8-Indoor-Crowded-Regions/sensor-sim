084655f4bb4cdcf504c88fe9561a466ea227ade2
TheMOHD3886 <65400638+TheMOHD3886@users.noreply.github.com>
2025-03-13 10:47:37 +0100

    feat: added sensor controller

---
+++ app/controllers/sensor_controller.py
@@ -0,0 +1,45 @@
+import bson.errors
+from fastapi import HTTPException
+from db.models.sensor import Sensor
+from app.utils.data_models.sensors import SensorModel
+import bson
+
+async def get_all_sensors() -> list[SensorModel]:
+  """Fetch all sensors from the database."""
+  try:
+    sensors = Sensor.objects()  # type: ignore
+    serialized_sensors = []
+
+    for sensor in sensors:
+      serialized_sensors.append({
+        "id": str(sensor.id),  # Convert ObjectId to string if necessary
+        "name": sensor.name,
+        "rooms": [str(room.id) for room in sensor.rooms],  # Extract room IDs
+        "movements": sensor.movements
+      })
+
+    return serialized_sensors
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+  
+async def get_sensor_by_id(sensor_id: str) -> SensorModel:
+  """Fetch a sensor by its id."""
+  validate_sensor_id(sensor_id)
+  try:
+    sensor = await fetch_sensor_by_id(sensor_id)
+    return sensor
+  except HTTPException:
+    raise
+  except Exception as e:
+    print(e)
+    raise HTTPException(status_code=500, detail=str(e))
+  
+def validate_sensor_id(sensor_id: str):
+  if not bson.ObjectId.is_valid(sensor_id):
+    raise HTTPException(status_code=400, detail='Invalid sensor id.')
+  
+async def fetch_sensor_by_id(sensor_id: str) -> SensorModel:
+  sensor = Sensor.objects(id=bson.ObjectId(sensor_id)).first()  # type: ignore
+  if not sensor:
+    raise HTTPException(status_code=404, detail='Sensor not found.')
+  return sensor
