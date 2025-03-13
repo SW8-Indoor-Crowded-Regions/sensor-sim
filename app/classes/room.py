efa311ce33a53f02112aa618f45d55976f14f235
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-13 08:06:26 +0100

    format: formatting + types for class fields

--- app/classes/room.py
+++ app/classes/room.py
@@ -1,15 +1,21 @@
-class Room ():
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from app.classes.sensor import Sensor
+
+
+class Room:
 	"""Represents a room in a building."""
-	from .sensor import Sensor
+
 	def __init__(self, room_info: dict, crowd_factor: float, area: float, sensors: list[Sensor]):
-		self.id = room_info["id"]
-		self.name = room_info["name"]
-		self.type = room_info["type"]
-		self.occupancy = 0
-		self.crowd_factor = crowd_factor
-		self.area = area
-		self.sensors = sensors
-	
+		self.id: int = room_info['id']
+		self.name: str = room_info['name']
+		self.type: str = room_info['type']
+		self.occupancy: int = 0
+		self.crowd_factor: float = crowd_factor
+		self.area: float = area
+		self.sensors: list[Sensor] = sensors
+
 	def add_occupants(self, occupants: int) -> None:
 		"""Increments the room's occupancy by the number of occupants.
 
@@ -17,7 +23,7 @@ class Room ():
 				occupants (int): The number of occupants to add to the room's occupancy.
 		"""
 		self.occupancy += occupants
-	
+
 	def remove_occupants(self, occupants: int) -> None:
 		"""Decrements the room's occupancy by the number of occupants given.
 
@@ -25,8 +31,8 @@ class Room ():
 				Exception: If the room's occupancy woulde be less than zero.
 		"""
 		if self.occupancy - occupants < 0:
-			raise Exception("Room occupancy cannot be less than zero. The room is empty.")
+			raise Exception('Room occupancy cannot be less than zero. The room is empty.')
 		self.occupancy -= occupants
 
 	def __str__(self) -> str:
-		return f"Room (id={self.id}, name={self.name}, type={self.type}, occupancy={self.occupancy}, crowdFactor={self.crowd_factor}, area={self.area}, sensors={['Sensor id: ' + sensor.id.__str__() for sensor in self.sensors]})"
+		return f'Room (id={self.id}, name={self.name}, type={self.type}, occupancy={self.occupancy}, crowdFactor={self.crowd_factor}, area={self.area}, sensors={["Sensor id: " + sensor.id.__str__() for sensor in self.sensors]})'
--- app/utils/heuristics.py
+++ app/utils/heuristics.py
@@ -1,11 +1,13 @@
 import random
 
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
 	from app.classes.visitor import Visitor
 	from app.classes.sensor import Sensor
 
-def choose_next_move(visitor: "Visitor") -> "Sensor | None":
+
+def choose_next_move(visitor: 'Visitor') -> 'Sensor | None':
 	"""Chooses the next move for the visitor.
 
 	Returns:
@@ -14,8 +16,8 @@ def choose_next_move(visitor: "Visitor") -> "Sensor | None":
 	movement_options = visitor.get_movement_options()
 
 	if not movement_options:
-		raise Exception("No movement options found for visitor:", visitor)
-	
+		raise Exception('No movement options found for visitor:', visitor)
+
 	# Chance to stay in the same room
 	if random.random() < 0.1:
 		return None
@@ -28,25 +30,28 @@ def choose_next_move(visitor: "Visitor") -> "Sensor | None":
 
 	# Choose a random sensor to move to. Chance should be lower for sensor leeding to previous room
 	return random.choices(movement_options, normalized_weights)[0]
-	
- 
-def get_weights(movement_options, visitor) -> list[float]:
+
+
+def get_weights(movement_options: list[Sensor], visitor: 'Visitor') -> list[float]:
 	"""Returns the weights for each movement option.
 
 	Args:
 			movement_options (list[Sensor]): The movement options for the visitor.
-
+			visitor (Visitor): The visitor.
 	Returns:
 			list[float]: The weights for each movement option.
 	"""
-	weights = []
-	previous_room_id = visitor.visited_rooms[-2].id if len(visitor.visited_rooms) > 1 else None
+	weights: list[float] = []
+	previous_room_id: int | None = (
+		visitor.visited_rooms[-2].id if len(visitor.visited_rooms) > 1 else None
+	)
 	for sensor in movement_options:
 		if previous_room_id and previous_room_id in [room.id for room in sensor.rooms]:
 			weights.append(0.1)
 		else:
 			weights.append(1)
 	return weights
+
 
 def should_create_visitor() -> bool:
 	"""Returns a boolean indicating whether a visitor should be created.
