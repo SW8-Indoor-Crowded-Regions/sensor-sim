9a982a863054bd354bf4b2ab7f1280546dfff117
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-17 15:41:55 +0100

    fix: merged get/fetch sensor by id

--- app/controllers/sensor_controller.py
+++ app/controllers/sensor_controller.py
@@ -17,20 +17,14 @@ async def get_sensor_by_id(sensor_id: str) -> SensorModel:
 	"""Fetch a sensor by its id."""
 	validate_sensor_id(sensor_id)
 	try:
-		sensor = await fetch_sensor_by_id(sensor_id)
-		return sensor
-	except HTTPException:
-		raise
+		sensor = Sensor.objects(id=bson.ObjectId(sensor_id)).no_dereference().first() # type: ignore
 	except Exception as e:
 		traceback.print_exc()
 		raise HTTPException(status_code=500, detail=str(e))
+	if not sensor:
+		raise HTTPException(status_code=404, detail='Sensor not found.')
+	return sensor
   
 def validate_sensor_id(sensor_id: str):
   if not bson.ObjectId.is_valid(sensor_id):
     raise HTTPException(status_code=400, detail='Invalid sensor id.')
-  
-async def fetch_sensor_by_id(sensor_id: str) -> SensorModel:
-  sensor = Sensor.objects(id=bson.ObjectId(sensor_id)).no_dereference().first()  # type: ignore
-  if not sensor:
-    raise HTTPException(status_code=404, detail='Sensor not found.')
-  return sensor
