416a5ba2732a59cfb263157cd4f2b49e42aaed81
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-17 13:49:59 +0100

    fix: updated sensor model to encode ObjectIds correctly

--- app/utils/data_models/sensors.py
+++ app/utils/data_models/sensors.py
@@ -1,10 +1,20 @@
 from pydantic import BaseModel
 from typing import List
+from bson import ObjectId
 
 class SensorModel(BaseModel):
-  id: str
-  rooms: List[str]
-  movements: List[int]
+	id: ObjectId
+	rooms: List[ObjectId]
+  
+	def __str__(self):
+		return f'Sensor (id={self.id}, rooms={self.rooms})'
+ 
+	class Config:
+		arbitrary_types_allowed = True
+		json_encoders = {
+			ObjectId: str,
+		}
+		  
 
 class SensorListModel(BaseModel):
   sensors: List[SensorModel]
