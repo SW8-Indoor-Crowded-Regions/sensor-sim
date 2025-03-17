2626e498e9adad79ac2cb9d932021ff1e50a71cb
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-17 09:32:47 +0100

    fix: deleted leftover comment

--- app/test/routes/test_rooms.py
+++ app/test/routes/test_rooms.py
@@ -15,7 +15,7 @@ def sample_room():
         "name": "Test Room",
         "type": "MEETING",
         "crowd_factor": 0.8,
-        "occupants": 10, # DELETE AFTERWARDS
+        "occupants": 10,
         "area": 25.5,
         "longitude": 40.7128,
         "latitude": -74.0060,
