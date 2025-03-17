7bd74ae4164841d0b47ca266d7a2d3346cebd0eb
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-17 13:49:21 +0100

    fix: added no_dereference to objects call for sensors

--- app/controllers/sensor_controller.py
+++ app/controllers/sensor_controller.py
@@ -3,43 +3,34 @@ from fastapi import HTTPException
 from db.models.sensor import Sensor
 from app.utils.data_models.sensors import SensorModel
 import bson
+import traceback
 
 async def get_all_sensors() -> list[SensorModel]:
-  """Fetch all sensors from the database."""
-  try:
-    sensors = Sensor.objects()  # type: ignore
-    serialized_sensors = []
-
-    for sensor in sensors:
-      serialized_sensors.append({ # pragma: no cover
-        "id": str(sensor.id),
-        "name": sensor.name,
-        "rooms": [str(room.id) for room in sensor.rooms],
-        "movements": sensor.movements
-      })
-
-    return serialized_sensors
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+	"""Fetch all sensors from the database."""
+	try:
+		sensors = Sensor.objects().no_dereference()  # type: ignore
+		return sensors
+	except Exception as e:
+		raise HTTPException(status_code=500, detail=str(e))
   
 async def get_sensor_by_id(sensor_id: str) -> SensorModel:
-  """Fetch a sensor by its id."""
-  validate_sensor_id(sensor_id)
-  try:
-    sensor = await fetch_sensor_by_id(sensor_id)
-    return sensor
-  except HTTPException:
-    raise
-  except Exception as e:
-    print(e)
-    raise HTTPException(status_code=500, detail=str(e))
+	"""Fetch a sensor by its id."""
+	validate_sensor_id(sensor_id)
+	try:
+		sensor = await fetch_sensor_by_id(sensor_id)
+		return sensor
+	except HTTPException:
+		raise
+	except Exception as e:
+		traceback.print_exc()
+		raise HTTPException(status_code=500, detail=str(e))
   
 def validate_sensor_id(sensor_id: str):
   if not bson.ObjectId.is_valid(sensor_id):
     raise HTTPException(status_code=400, detail='Invalid sensor id.')
   
 async def fetch_sensor_by_id(sensor_id: str) -> SensorModel:
-  sensor = Sensor.objects(id=bson.ObjectId(sensor_id)).first()  # type: ignore
+  sensor = Sensor.objects(id=bson.ObjectId(sensor_id)).no_dereference().first()  # type: ignore
   if not sensor:
     raise HTTPException(status_code=404, detail='Sensor not found.')
   return sensor
