f0785cb8f911015979a88f2de0bc1c0a6c534bda
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-04-04 14:44:59 +0200

    fix: conversion of id in load objects

--- app/utils/load_objects.py
+++ app/utils/load_objects.py
@@ -11,6 +11,7 @@ def load_rooms() -> list[Room]:
 	rooms = []
 	room_data = RoomModel.objects()  # type: ignore
 	for room in room_data:
+		room['id'] = room['_id']
 		rooms.append(Room(room, room['crowd_factor'], room['popularity_factor'], room['area'], []))
 	return rooms
 
